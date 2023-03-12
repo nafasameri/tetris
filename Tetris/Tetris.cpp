0837465e68217146c91fce3267670c1a6fb8b862
Nafas <nafiseh.ameri@partsoftware.com>
2023-03-12 21:41:13 +0330

    final

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,7 +1,4 @@
-// Tetris.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "conio.h"
 #include "iostream"
 #include "windows.h"
@@ -9,11 +6,9 @@
 using namespace std;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-
 void gotoxy(int x, int y)
 {
 	COORD pos;
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (INVALID_HANDLE_VALUE != hConsole)
 	{
 		pos.X = x;
@@ -23,163 +18,363 @@ void gotoxy(int x, int y)
 }
 
 
+class Time
+{
+	int sec;
+	int min;
+	int hour;
+public:
+	Time()
+	{
+		sec = 0;
+		min = 0;
+		hour = 0;
+	}
+
+	Time &operator++()
+	{
+		sec++;
+		if (sec > 59)
+		{
+			min++;
+			if (min > 59)
+			{
+				hour++;
+				min = 0;
+			}
+			sec = 0;
+		}
+		return *this;
+	}
+
+	void PrintTime()
+	{
+		cout << hour << ":" << min << ":" << sec;
+	}
+};
+
 class Table
 {
-	int w;
-	int h;
-	int **state;
+	int w;//ستون
+	int len;//سطر
+	int CharTable;
+	int state[20][20];//ارایه حالت که بود و نبود شکل ها را بررسی میکند
 public:
-	Table(int n,int m)
+	Table(int m,int h)
 	{
-		w = n;
-		h = m;
-		state = new int*[w];
+		w = m;
+		len = h;
+		CharTable = 219;
+		/*state = new int*[w];
 		for (int i = 0; i < w; i++)
 			state[i] = new int[h];
-		state = { 0 };
-		for (int i = 0; i < w; i++)
+		state = { 0 };*/
+		for (int i = 0; i < len; i++)
+			for (int j = 0; j < w; j++)
+				state[i][j] = 0;//همه ی ارایه را صفر میکند
+		for (int i = 0; i < len; i++)//سطر اول جدول را یک میکند
 			state[0][i] = 1;
-		for (int i = 0; i < w; i++)
-			state[h - 1][i] = 1;
-		for (int i = 0; i < h; i++)
+		for (int i = 0; i < len; i++)//سطر اخر
+			state[w - 1][i] = 1;
+		for (int i = 0; i < w; i++)//ستون چپ
 			state[i][0] = 1;
-		for (int i = 0; i < h; i++)
-			state[i][w - 1] = 1;
+		//for (int i = 0; i < w; i++)//ستون راست
+			//state[i][len - 1] = 1;
 	}
+
+	//ارایه حالت را اگ شکل داشت پاک میشد صفر میکنه اگ داشت چاپ میشد یک میکنه
+	void setState0(int x, int y) { state[y][x] = 0; }
+	void setState1(int x, int y) { state[y][x] = 1; }
 
 	void Bax()
 	{
-		//SetConsoleTextAttribute(hConsole, 13);
-		for (int i = 0; i < w; i++)
-			cout << (char)219;
+		SetConsoleTextAttribute(hConsole, 5);
+		for (int i = 0; i < len; i++)//x
+			cout << (char)CharTable;
 		cout << endl;
-		for (int i = 1; i < h; i++)
+		for (int i = 2; i < w; i++)//y
 		{
-			cout << (char)219;
-			for (int j = 0; j < w - 2; j++)
+			cout << (char)CharTable;
+			for (int j = 2; j < len; j++)//x
 				cout << " ";
-			cout << (char)219 << endl;
+			cout << (char)CharTable << endl;
 		}
-		for (int i = 0; i < w; i++)
-			cout << (char)219;
+		for (int i = 0; i < len; i++)//y
+			cout << (char)CharTable;
 	}
 
-	bool IsInTable(int x, int y)
+	bool IsInTable(int y, int x)
 	{
-		/*if (x < 2 || x > w-1 || y > h-3)
-			return false;
-		else
-			return true;*/
-		if (state[x][y] == 0)
-			return true;
-		else if (state[x][y + 1] == 0)
-			return true;
-		else if (state[x + 1][y] == 0)
-			return true;
-		else if (state[x - 1][y] == 0)
-			return true;
-		else if (state[x + 1][y + 1] == 0)
-			return true;
-		else if (state[x - 1][y + 1] == 0)
-			return true;
-		else
-			return false;
+		if (y > 3 && y < len - 3 || x < w - 3)//چک میکنه تو جدول هست یا ن
+		{
+			if (state[x][y + 2] == 1)// اینام چک میکنن ک دور اون شکل شکل دیگ ای هست یا ن
+				return false;
+			//if (state[x - 2][y + 1] == 1)
+				//return false;
+			//if (state[x + 1][y + 1] == 1)
+				//return false;
+			if (state[x + 2][y + 1] == 1)
+				return false;
+			//if (state[x + 2][y + 2] == 1)
+				//return false;
+
+			if (state[x][y + 2] == 0)
+				return true;
+			if (state[x - 2][y + 1] == 0)
+				return true;
+			if (state[x + 1][y + 1] == 0)
+				return true;			
+			if (state[x + 2][y + 1] == 0)
+				return true;
+			//if (state[x + 2][y + 2] == 0)
+				//return true;
+
+			/*
+			if (state[x][y + 2] == 1)// اینام چک میکنن ک دور اون شکل شکل دیگ ای هست یا ن
+				return false;
+			else if (state[x][y + 2] == 0)
+				return true;
+			
+			//if (state[x -1][y + 2] == 1)
+				//return false;
+			//else if (state[x - 1][y + 2] == 0)
+				//return true;
+			
+			//if (state[x + 1][y + 1] == 1)
+				//return false;
+			//else if (state[x + 1][y + 1] == 0)
+				//return true;
+			
+			//if (state[x + 2][y + 1] == 1)
+				//return false;
+			//else if (state[x + 2][y + 1] == 0)
+				//return true;
+					
+			//if (state[x + 2][y + 2] == 1)
+				//return false;
+			//else if (state[x + 2][y + 2] == 0)
+				//return true;
+			*/
+		}
+		return false;
+	}
+
+	bool Shift()//تابعی که چک میکنه اگر یک خط پر شد اونو پاک کنه
+	{
+		bool flag = false;
+		int count = 0;
+		for (int i = 1; i < w - 1; i++)
+		{
+			for (int j = 0; j < len; j++)
+				if (state[i][j] == 1)
+					count++;
+			if (count == len - 1)//اگر تعداد یک ها با سطرهاش یا طولش برابر بود حالا بیاد شیفتش بده
+			{
+				flag = true;
+				while (i > 1)
+				{
+					for (int k = 0; k < len; k++)
+						state[i][k] = state[i - 1][k];//تمام مقادیر خط بالاییشو ببره پایین روی اون یک ها
+					i--;
+				}
+			}
+			count = 0;
+		}
+		return flag;
+	}
+
+	void DeleteAfterShift()
+	{
+		for (int i = 0; i < w; i++)
+			for (int j = 0; j < len; j++)
+				if (state[i][j] == 1)
+				{
+					SetConsoleTextAttribute(hConsole, 5);
+					gotoxy(j, i);
+					cout << (char)CharTable;
+				}
+				else
+				{
+					gotoxy(j, i);
+					cout << ' ';
+				}
+		for (int i = 0; i < w; i++)//ستون راست
+		{
+			gotoxy(len - 1,i);
+			cout << (char)CharTable;
+		}
+	}
+
+	bool EndGame()
+	{
+		for (int i = 1; i < w-1; i++)
+			if (state[1][i] == 1)//چک میکنه ک اگر سط دوم توی ارایه حتی اگر یکدونش یک شده ینی دیگ جایی نداره ک شکل بعدی بیاد پس از بازی میاد بیرون
+				return true;
+		return false;
 	}
 };
 
 class Shape
 {
 protected:
-	int X;
+	int X;//مختصات شکل
 	int Y;
-	Table table;
+	int state[3][3];
+	int CharShape;
+	Table *table;
 
 public:
-	Shape(int x, int y, Table t):table(t)
+	Shape(int x, int y, Table *t)//:table(t)
 	{
 		X = x;
 		Y = y;
+		CharShape = 178;
+		table = t;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				state[i][j] = 0;
 	}
+
+	int getX() { return X; }
+	int getY() { return Y; }
 
 	void Move(char ch)
 	{
-		if (table.IsInTable(X, Y))
+		if (table->IsInTable(X, Y))
+		{
+			if (GetAsyncKeyState(VK_LEFT))X--;
+			if (GetAsyncKeyState(VK_RIGHT))X++;
+			if (GetAsyncKeyState(VK_DOWN))Y++;
+		}
+		/*if (table->IsInTable(X, Y))
 			switch (ch)
 			{
-				case '4':X--;
+				case 'a':X--;
 					break;
-				case '6':X++;
+				case 'd':X++;
 					break;
-				case '2':Y++;
+				case 's':Y++;
 					break;
-			}
+			}*/
 	}
 
 	Shape &operator++(int)
 	{
-		if (table.IsInTable(X, Y))
+		if (table->IsInTable(X, Y))
 			Y++;
 		return *this;
 	}
 
-	virtual void Print() = 0;
-	virtual void UnPrint() = 0;
-	virtual void Rotatoin(char ch) {}
+	virtual void Print() = 0;//تابع چاپ
+	virtual void UnPrint() = 0;//تابع حذف از صفحه
+	virtual void Rotatoin(char ch) { }//چرخش
 
 };
 
 class S :public Shape
 {
 public:
-	S(int x, int y, Table t) :Shape(x, y, t) {}
-
+	S(int x, int y, Table *t) :Shape(x, y, t) 
+	{
+		state[0][1] = state[1][1] = state[0][2] = state[1][2] = 1;
+	}
+	
 	void Print()
 	{
-		gotoxy(X, Y); cout << (char)219;
-		gotoxy(X, Y + 1); cout << (char)219;
-		gotoxy(X + 1, Y); cout << (char)219;
-		gotoxy(X + 1, Y + 1); cout << (char)219;
+		SetConsoleTextAttribute(hConsole, 4);
+		gotoxy(X, Y); cout << (char)CharShape; table->setState1(X, Y);
+		gotoxy(X, Y + 1); cout << (char)CharShape; table->setState1(X, Y + 1);
+		gotoxy(X + 1, Y); cout << (char)CharShape; table->setState1(X + 1, Y);
+		gotoxy(X + 1, Y + 1); cout << (char)CharShape; table->setState1(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+			gotoxy(X, Y + i);
+			for (int j = 0; j < 3; j++)
+				if (state[i][j] == 1)
+				{
+					cout << (char)CharShape;
+					table->setState1(X , Y + j);
+				}
+				
+		}*/
 	}
 
 	void UnPrint()
 	{
-		gotoxy(X, Y); cout << " ";
-		gotoxy(X, Y + 1); cout << " ";
-		gotoxy(X + 1, Y); cout << " ";
-		gotoxy(X + 1, Y + 1); cout << " ";
+		gotoxy(X, Y); cout << " "; table->setState0(X, Y);
+		gotoxy(X, Y + 1); cout << " "; table->setState0(X, Y + 1);
+		gotoxy(X + 1, Y); cout << " "; table->setState0(X + 1, Y);
+		gotoxy(X + 1, Y + 1); cout << " "; table->setState0(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+			gotoxy(X, Y + i);
+			for (int j = 0; j < 3; j++)
+				if (state[i][j] == 1)
+				{
+					cout << " ";
+					table->setState0(X , Y + j);
+				}
+		}*/
 	}
-
-
 };
 
-class L :public Shape
+class L
+	:public Shape
 {
 public:
-	L(int x, int y, Table t) :Shape(x, y, t) {}
+	L(int x, int y, Table *t) :Shape(x, y, t) 
+	{
+		state[0][1] = state[1][1] = state[1][2] = 1;
+	}
 
 	void Print()
 	{
-		gotoxy(X, Y); cout << (char)178;
-		gotoxy(X, Y + 1); cout << (char)178;
-		gotoxy(X, Y + 2); cout << (char)178;
-		gotoxy(X + 1, Y + 2); cout << (char)178;
+		SetConsoleTextAttribute(hConsole, 1);
+		gotoxy(X, Y); cout << (char)CharShape; table->setState1(X, Y);
+		gotoxy(X, Y + 1); cout << (char)CharShape; table->setState1(X, Y + 1);
+		gotoxy(X + 1, Y + 1); cout << (char)CharShape; table->setState1(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+
+			for (int j = 0; j < 3; j++)
+			{
+				gotoxy(X - 1 + i, Y + j);
+				if (state[i][j] == 1)
+				{
+					cout << (char)CharShape;
+					table->setState1(X - 1 + i, Y + j);
+				}
+			}
+		}*/
 	}
 
 	void UnPrint()
 	{
-		gotoxy(X, Y); cout << " ";
-		gotoxy(X, Y + 1); cout << " ";
-		gotoxy(X, Y + 2); cout << " ";
-		gotoxy(X + 1, Y + 2); cout << " ";
+		gotoxy(X, Y); cout << " "; table->setState0(X, Y);
+		gotoxy(X, Y + 1); cout << " "; table->setState0(X, Y + 1);
+		gotoxy(X + 1, Y + 1); cout << " "; table->setState0(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+
+			for (int j = 0; j < 3; j++)
+			{
+				gotoxy(X - 1 + i, Y + i);
+				if (state[i][j] == 1)
+				{
+					cout << " ";
+					table->setState0(X - 1 + i, Y + j);
+				}
+			}
+		}*/
 	}
 
 	void Rotation(char ch)
 	{
 		switch (ch)
 		{
-		case 'a':
+		case '4': state[1][2] = 0; state[1][0] = 1;
 			break;
-		case 'd':
+		case '6': state[0][1] = 0; state[2][2] = 1;
 			break;
 		}
 	}
@@ -188,65 +383,123 @@ public:
 class Z :public Shape
 {
 public:
-	Z(int x, int y,Table t) :Shape(x, y,t){}
+	Z(int x, int y,Table *t) :Shape(x, y,t)
+	{
+		state[0][0] = state[0][1] = state[1][1] = state[1][2] = 1;
+	}
 
 	void Print()
 	{
-		gotoxy(X, Y); cout << (char)219;
-		gotoxy(X + 1, Y); cout << (char)219;
-		gotoxy(X + 1, Y + 1); cout << (char)219;
-		gotoxy(X + 2, Y + 1); cout << (char)219;
+		SetConsoleTextAttribute(hConsole, 2);
+		gotoxy(X, Y); cout << (char)CharShape; table->setState1(X, Y);
+		gotoxy(X - 1, Y); cout << (char)CharShape; table->setState1(X - 1, Y);
+		gotoxy(X, Y + 1); cout << (char)CharShape; table->setState1(X, Y + 1);
+		gotoxy(X + 1, Y + 1); cout << (char)CharShape; table->setState1(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << (char)CharShape;
+		table->setState1(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void UnPrint()
 	{
-		gotoxy(X, Y); cout << " ";
-		gotoxy(X + 1, Y); cout << " ";
-		gotoxy(X + 1, Y + 1); cout << " ";
-		gotoxy(X + 2, Y + 1); cout << " ";
+		gotoxy(X, Y); cout << " "; table->setState0(X, Y);
+		gotoxy(X - 1, Y); cout << " "; table->setState0(X - 1, Y);
+		gotoxy(X, Y + 1); cout << " "; table->setState0(X, Y + 1);
+		gotoxy(X + 1, Y + 1); cout << " "; table->setState0(X + 1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << " ";
+		table->setState0(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void Rotation(char ch)
 	{
 		switch (ch)
 		{
-		case 'a':
+		case '4':state[0][0] = state[1][2] = 0;
+			state[1][0] = state[2][0] = 1;
 			break;
-		case 'd':
+		case '6':state[0][0] = 0;
+			state[1][2] = state[2][2] = 1;
 			break;
 		}
 	}
 	
 };
 
-class I :public Shape
+class U :public Shape
 {
 public:
-	I(int x, int y, Table t) :Shape(x, y, t) {}
+	U(int x, int y, Table *t) :Shape(x, y, t) 
+	{
+		state[0][0] = state[1][0] = state[1][1] = state[0][2] = state[1][2] = 1;
+	}
 
 	void Print()
 	{
-		gotoxy(X, Y); cout << (char)219;
-		gotoxy(X, Y + 1); cout << (char)219;
-		//gotoxy(X, Y + 2); cout << (char)219;
-		//gotoxy(X, Y + 3); cout << (char)219;
+		SetConsoleTextAttribute(hConsole, 3);
+		//gotoxy(X, Y); cout << (char)CharShape; table->setState1(X, Y);
+		//gotoxy(X, Y + 1); cout << (char)CharShape; table->setState1(X, Y + 1);
+		gotoxy(X, Y+1); cout << (char)CharShape; table->setState1(X, Y+1);
+		gotoxy(X-1, Y); cout << (char)CharShape; table->setState1(X-1, Y);
+		gotoxy(X+1, Y); cout << (char)CharShape; table->setState1(X+1, Y);
+		gotoxy(X - 1, Y + 1); cout << (char)CharShape; table->setState1(X-1, Y+1);
+		gotoxy(X+1, Y+1); cout << (char)CharShape; table->setState1(X+1, Y+1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << (char)CharShape;
+		table->setState1(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void UnPrint()
 	{
-		gotoxy(X, Y); cout << " ";
-		gotoxy(X, Y + 1); cout << " ";
-		//gotoxy(X, Y + 2); cout << " ";
-		//gotoxy(X, Y + 3); cout << " ";
+		//gotoxy(X, Y); cout << " "; table->setState0(X, Y);
+		//gotoxy(X, Y + 1); cout << " "; table->setState0(X, Y + 1);
+		gotoxy(X, Y+1); cout << " "; table->setState0(X, Y+1);
+		gotoxy(X-1, Y); cout << " "; table->setState0(X-1, Y);
+		gotoxy(X+1, Y); cout << " "; table->setState0(X+1, Y);
+		gotoxy(X-1, Y+1); cout << " "; table->setState0(X-1, Y+1);
+		gotoxy(X+1, Y+1); cout << " "; table->setState0(X+1, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << " ";
+		table->setState0(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void Rotation(char ch)
 	{
 		switch (ch)
 		{
-		case 'a':
+		case '4':state[0][0] = state[1][0] = state[1][2] = 0;
+			state[0][1] = state[2][1] = state[2][2] = 1;
 			break;
-		case 'd':
+		case '6':state[1][0] = state[0][2] = state[1][2] = 0;
+			state[0][1] = state[2][0] = state[2][1] = 1;
 			break;
 		}
 	}
@@ -255,90 +508,122 @@ public:
 class T :public Shape
 {
 public:
-	T(int x, int y, Table t) :Shape(x, y, t) {}
+	T(int x, int y, Table *t) :Shape(x, y, t) 
+	{
+		state[0][1] = state[1][1] = state[1][2] = state[1][0] = 1;
+	}
 
 	void Print()
 	{
-		gotoxy(X - 1, Y); cout << (char)219;
-		gotoxy(X, Y); cout << (char)219;
-		gotoxy(X + 1, Y); cout << (char)219;
-		gotoxy(X, Y + 1); cout << (char)219;
+		SetConsoleTextAttribute(hConsole, 6);
+		gotoxy(X - 1, Y + 1); cout << (char)CharShape; table->setState1(X - 1, Y + 1);
+		gotoxy(X, Y); cout << (char)CharShape; table->setState1(X, Y);
+		gotoxy(X + 1, Y + 1); cout << (char)CharShape; table->setState1(X + 1, Y + 1);
+		gotoxy(X, Y + 1); cout << (char)CharShape; table->setState1(X, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << (char)CharShape;
+		table->setState1(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void UnPrint()
 	{
-		gotoxy(X - 1, Y); cout << " ";
-		gotoxy(X, Y); cout << " ";
-		gotoxy(X + 1, Y); cout << " ";
-		gotoxy(X, Y + 1); cout << " ";
+		gotoxy(X - 1, Y + 1); cout << " "; table->setState0(X - 1, Y + 1);
+		gotoxy(X, Y); cout << " "; table->setState0(X, Y);
+		gotoxy(X + 1, Y + 1); cout << " "; table->setState0(X + 1, Y + 1);
+		gotoxy(X, Y + 1); cout << " "; table->setState0(X, Y + 1);
+		/*for (int i = 0; i < 3; i++)
+		{
+		gotoxy(X, Y + i);
+		for (int j = 0; j < 3; j++)
+		if (state[i][j] == 1)
+		{
+		cout << " ";
+		table->setState0(X + i, Y + j);
+		}
+		}*/
 	}
 
 	void Rotation(char ch)
 	{
 		switch (ch)
 		{
-		case 'a':
+		case '4':state[1][2] = 0;
+			state[2][1] = 1;
 			break;
-		case 'd':
+		case '6':state[1][0] = 0;
+			state[2][1] = 1;
 			break;
 		}
 	}
 };
 
 
-
-void main()
+void _tmain()
 {
-	Table table(35, 30);
+	cout << "Moves Shapes:" << endl << endl << " " << (char)27 << "\t " << (char)26 << endl << "     " << (char)25 << endl << endl << endl << "START...";
+	while (true)//این حلقه زمانی بیرون میره ک کاربر حتما اسپیس بزنه تا بازی شروع بشه
+		if (_getch() == (char)32)
+			break;
+	system("cls");
+	Time time;
+	Table table(20, 20);
+	Shape *shape = new L(9, 1, &table);
 	table.Bax();
-	char ch;
-	Shape *shape;
-	
-
-	int number = rand() % 5;
-	switch (number)
-	{
-		case 0:
-		{
-			S s(15, 2, table);
-			shape = &s;
-			break;
-		}
-		case 1:
-		{
-			L l(15, 2, table);
-			shape = &l;
-			break;
-		}
-		case 2:
-		{
-			Z z(15, 2, table);
-			shape = &z;
-			break;
-		}
-		case 3:
-		{
-			T t(15, 2, table);
-			shape = &t;
-			break;
-		}
-		case 4:
-		{	I i(15, 2, table);
-		shape = &i;
-		break;
-		}
-	}
+	int score = 0;//امتیاز
+	int timer = 0;//تنظیم کننده ی سرعت تایمر است
 	while (true)
 	{
-		if (_kbhit()) 
+		if (_kbhit())//زمانی ک کاربرد کاراکتری وارد کرد این شط انجاام میشه
 		{
+			char ch = _getch();
 			shape->UnPrint();
-			ch = _getch();
-			shape->Move(ch);
+			shape->Move(ch); // و این کاراکتر به تابع حرکت ارسال میشه 
+			//shape->Rotatoin(ch);
+			shape->Print();
+			if (ch == (char)32)//اگر اسپیس زد بازی استپ بشه تا زمانی ک یدونه کاراکتر بزنه
+				_getch();
 		}
 		shape->UnPrint();
-		shape++;
+		(*shape)++;
 		shape->Print();
-		Sleep(500);
+		if (!table.IsInTable(shape->getX(), shape->getY()))//اگر شکل به جایی رسید ک نمیتوانست حرکت کند و ثابت ماند شکل جدیدی وارد بازی میکنیم
+		{
+			int random = rand() % 5;//تابع رندم: یک عدد از بین 0تا 4 انتخاب میکند آن عددرا در نامبر ذخیره میکنیم و متناسب با ان شکل جدید را به شیپ اختصاص میدهیم
+			switch (random)
+			{
+				case 0: shape = new S(9, 1, &table); break;
+				case 1: shape = new U(9, 1, &table); break;
+				case 2: shape = new T(9, 1, &table); break;
+				case 3: shape = new L(9, 1, &table); break;
+				case 4: shape = new Z(9, 1, &table); break;
+			}
+		}
+		if (timer % 4 == 0)//این شرط تامیر را تنظیم میکند
+		{
+			SetConsoleTextAttribute(hConsole, 14);
+			gotoxy(7, 24);
+			time.PrintTime();
+			++time;
+		}
+		if (table.Shift())// اگر یک خط پرشد آن خط را پاک میکند
+		{
+			table.DeleteAfterShift();
+			score++;
+		}
+		if (table.EndGame())// اگر ب انتهای بازی رسیدیم یعنی حتی اگر یدونه یک در سطر اول جدول بود از بازی خارج میشود
+		{
+			SetConsoleTextAttribute(hConsole, 12); gotoxy(5, 10); cout << "Game Over!";
+			_getch(); _getch();
+			exit(0);
+		}	
+		gotoxy(6, 22); cout << "Score: " << score;
+		Sleep(250); timer++;
 	}
 }
